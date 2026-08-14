31800cb0f2598b949682c20d024b51e4b7eae0c3
grivas9735 <gonza.97.35@gmail.com>
2026-08-13 21:24:48 -0300

    .

---
+++ Metin2Bot/Screenshots/ScreenshotHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Metin2Bot.Screenshots
+{
+    public static class ScreenshotHelper
+    {
+        public enum Captura 
+        {
+            Fragmentos, 
+            Login,
+            ChampSelect,
+            Revivir,
+            InsideGame
+        }
+
+        public static void TakePicture(Metin2 metin, Captura captura)
+        {
+            switch (captura)
+            {
+                case Captura.Fragmentos:
+                    ScreenShot.SacarScreenshotFragmentos(metin);
+                    break;
+                case Captura.Login:
+                    ScreenShot.SacarScreenshotPantallaLogin(metin.StartX, metin.StartY);
+                    break;
+                case Captura.InsideGame:
+                    ScreenShot.SacarScreenshotInsideGame(metin.StartX, metin.StartY);
+                    break;
+                case Captura.Revivir:
+                    ScreenShot.SacarScreenshotEstaMuerto(metin.StartX, metin.StartY);
+                    break;
+                    
+            }
+        }
+
+        public static void GetPictureText()
+        {
+
+        }
+    }
+}
