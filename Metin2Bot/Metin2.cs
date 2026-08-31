fbb4816099ef20c7078246abb6a2292294fa2b92
grivas9735 <gonza.97.35@gmail.com>
2026-08-31 00:02:34 -0300

    .

--- Metin2Bot/Metin2.cs
+++ Metin2Bot/Metin2.cs
@@ -31,7 +31,7 @@ namespace Metin2Bot
 
         public TextRegion? TextRegion { get; set; }
 
-        public TimeSpan timerPocionRoja = TimeSpan.FromSeconds(2);
+        public TimeSpan timerPocionRoja = TimeSpan.FromSeconds(1);
         public DateTime timerPocionRojaDate = DateTime.Now.AddDays(-1);
 
         public TimeSpan timerPocionAzul = TimeSpan.FromSeconds(85);
