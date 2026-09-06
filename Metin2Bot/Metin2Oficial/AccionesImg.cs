7c4ee94b65573b245503b97180188616f7a431c4
grivas9735 <gonza.97.35@gmail.com>
2026-09-06 20:26:43 -0300

    Update AccionesImg.cs

--- Metin2Bot/Metin2Oficial/AccionesImg.cs
+++ Metin2Bot/Metin2Oficial/AccionesImg.cs
@@ -17,7 +17,7 @@ namespace Metin2Bot.Metin2Oficial
         private static List<string> ItemsSiempre = new List<string>()
         { "weiliao", "arte guerra", "ao zi", "arte guerra", "arteguerra",
           "bola", "polimorf", "wu zi", "luz luna", "luzluna", "pendiente",
-          "collar", "bota", "zapato"
+          "collar", "bota", "zapato", "morada"
         };
 
         public static List<string> ListaItemsAgarrar()
