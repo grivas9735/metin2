8654657187bd97ec600cb704b2a235779da31e64
grivas9735 <gonza.97.35@gmail.com>
2026-09-12 14:46:58 -0300

    .

--- Metin2Bot/Metin2Oficial/Metin1600x900.cs
+++ Metin2Bot/Metin2Oficial/Metin1600x900.cs
@@ -288,7 +288,7 @@ namespace Metin2Bot.Metin2Oficial
                     10);
                 await btn.PocionRoja(10);
                 await Task.Delay(1000);
-                //await btn.AgarrarItems();
+
                 metin.TextRegion = null;
                 metin.timerAutocazaDate = DateTime.Now.AddDays(-1);
                 return;
