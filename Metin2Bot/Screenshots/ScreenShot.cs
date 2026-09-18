c3e87bccdad366914346b2b9c96d77a267b1340a
grivas9735 <gonza.97.35@gmail.com>
2026-09-17 22:53:16 -0300

    .

--- Metin2Bot/Screenshots/ScreenShot.cs
+++ Metin2Bot/Screenshots/ScreenShot.cs
@@ -2,9 +2,12 @@
 {
     public static class ScreenShot
     {
-        public static Bitmap SacarScreenshotTiendaGeneralAbiertaBM(Metin2 metin)
+        // Método privado genérico para evitar duplicación
+        private static Bitmap CapturarRegion(Rectangle captureArea)
         {
-            var captureArea = Resolution.RectScreenshotTiendaGeneralAbierta(metin);
+            if (captureArea.Width <= 0 || captureArea.Height <= 0)
+                return null!;
+
             Bitmap screenshot = new(captureArea.Width, captureArea.Height);
             using (Graphics g = Graphics.FromImage(screenshot))
             {
@@ -13,103 +16,15 @@
             return screenshot;
         }
 
-        public static Bitmap SacarScreenshotTextoInventarioBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotTextoInventario(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotInventarioBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotInventario(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotFragmentosBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotFragmentos(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotNPCBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotNPC(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotCoordenadasBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotCoordenadas(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotEstaMuertoBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotEstaMuerto(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotMisionAlquimiaBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotMisionAlquimia(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotPantallaLoginBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotPantallaLogin(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
-
-        public static Bitmap SacarScreenshotChampSelectBM(Metin2 metin)
-        {
-            var captureArea = Resolution.RectScreenshotChampSelect(metin);
-            Bitmap screenshot = new(captureArea.Width, captureArea.Height);
-            using (Graphics g = Graphics.FromImage(screenshot))
-            {
-                g.CopyFromScreen(captureArea.Location, Point.Empty, captureArea.Size);
-            }
-            return screenshot;
-        }
+        public static Bitmap SacarScreenshotTiendaGeneralAbiertaBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotTiendaGeneralAbierta(metin));
+        public static Bitmap SacarScreenshotTextoInventarioBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotTextoInventario(metin));
+        public static Bitmap SacarScreenshotInventarioBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotInventario(metin));
+        public static Bitmap SacarScreenshotFragmentosBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotFragmentos(metin));
+        public static Bitmap SacarScreenshotNPCBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotNPC(metin));
+        public static Bitmap SacarScreenshotCoordenadasBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotCoordenadas(metin));
+        public static Bitmap SacarScreenshotEstaMuertoBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotEstaMuerto(metin));
+        public static Bitmap SacarScreenshotMisionAlquimiaBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotMisionAlquimia(metin));
+        public static Bitmap SacarScreenshotPantallaLoginBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotPantallaLogin(metin));
+        public static Bitmap SacarScreenshotChampSelectBM(Metin2 metin) => CapturarRegion(Resolution.RectScreenshotChampSelect(metin));
     }
 }
